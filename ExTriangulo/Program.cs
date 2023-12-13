c13063ca505aa125a5af1b05d90dba88cb8052aa
Joao Fernandes <a11493@aemaximinos.edu.pt>
2023-12-13 14:24:00 +0000

    ExTriangulo

---
+++ ExTriangulo/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ExTriangulo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Gestão de triângulos. Insira os dados pedidos:");
+
+            Console.Write("1º Triângulo: Lado A:  ");
+            double a = double.Parse(Console.ReadLine());
+            Console.Write("1º Triângulo: Lado B:  ");
+            double b = double.Parse(Console.ReadLine());
+            Console.Write("1º Triângulo: Lado C:  ");
+            double c = double.Parse(Console.ReadLine());
+
+            Triangulo t1 = new Triangulo(a, b, c);
+            if(t1.ETriangulo())
+                Console.WriteLine($"{t1.ToString}");
+            else
+                Console.WriteLine("As medidas inseridas não formam um triângulo");
+
+            Console.Write("2º Triângulo: Lado A:  ");
+            a = double.Parse(Console.ReadLine());
+            Console.Write("2º Triângulo: Lado B:  ");
+            b = double.Parse(Console.ReadLine());
+            Console.Write("2º Triângulo: Lado C:  ");
+            c = double.Parse(Console.ReadLine());
+
+            Triangulo t2 = new Triangulo(a, b, c);
+            Console.WriteLine($"{t2.ToString}");
+        }
+    }
+}
---
+++ ExTriangulo/Triangulo.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExTriangulo
+{
+    internal class Triangulo
+    {
+        private double A;
+        private double B;
+        private double C;
+
+        public Triangulo(double a, double b, double c)
+        {
+            A = a;
+            B = b;
+            C = c;
+        }
+
+        public bool ETriangulo()
+        {
+            if ((A < (B + C) && (B < (A + C) && C < (B + A))
+                return true;
+            return false;
+        }
+
+        public string TipoTriangulo()
+        {
+            if (A == B && B == C) return "Equilátero";
+            else if (A == B || B == C || C == A) return "Isósceles";
+            else return "Escaleno";
+        }
+
+        public double AreaTriangulo()
+        {
+            double p = (A + B + B) / 2;
+            return Math.Sqrt(p * (p - A) * (p - B) * (p - C));
+        }
+
+        public double PerimetroTriangulo()
+        {
+            return (A + B + C);
+        }
+
+        public override string ToString()
+        {
+            return $"Dados do Triângulo:  " +
+                $"\n\tLado A: {A:f2}" +
+                $"\n\tLado B: {B:f2}" +
+                $"\n\tLado C: {C:f2}" +
+                $"\n\tTipo: {TipoTriangulo()}" +
+                $"\n\tArea: {AreaTriangulo():f2}" +
+                $"\n\tPerimetro: {PerimetroTriangulo():f2}";
+        }
+    }
+}
